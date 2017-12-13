3c400df136d956f93d7d7b5479640b0783dd9d90
Revathi-Kundurthi <revathi.kundurthi@gmail.com>
2017-12-13 17:24:31 -0500

    arraylist without generics

--- workspace/ConceptOfTheDay/src/com/conceptoftheday/list/ArrayListDemo.java
+++ workspace/ConceptOfTheDay/src/com/conceptoftheday/list/ArrayListDemo.java
@@ -6,17 +6,22 @@ public class ArrayListDemo {
 
 	public static void main(String[] args) {
 	
+		//creating an arraylist object
 		ArrayList<String> list = new ArrayList<String>();
+		
+		//adding elements into arraylist
 		list.add("Revathi");
 		list.add("Pavani");
 		list.add("Java");
 		
+		//getting the size of arraylist
 		System.out.println(list.size());
 		list.add("Siri");
 		list.add("Sravani");
 		
 		System.out.println(list.size());
 		
+		//removing an element at index 1 from the arraylist
 		list.remove(1);
 		
 		System.out.println(list.size());
@@ -25,7 +30,27 @@ public class ArrayListDemo {
 		list.add("");
 		
 		System.out.println(list.size());
- 
+		
+		//creating an arraylist without generics
+		ArrayList al = new ArrayList();
+		
+		//adding string
+		al.add("String");
+		
+		//adding float
+		al.add("25.3");
+		
+		//adding integer
+		al.add("85");
+		
+		//adding float wrapper type 
+		al.add(new Float(25.3));
+		
+		//adding wrapper long type
+		al.add(new Long(85));
+		
+		System.out.println(list);
+		System.out.println(al);
 	}
 
 }
